23cc564f0916f6bcb268e9dda11792bfd9c1beb0
mrifkiw <61343742+mrifkiw@users.noreply.github.com>
2021-01-20 12:29:39 +0700

    initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\bwastore-laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\bwastore-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\hdrplnt\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\hdrplnt\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,60 +1,41 @@
-<nav
-    class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
-    data-aos="fade-down"
->
+<nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top" data-aos="fade-down">
     <div class="container">
-    <a href="{{ route('home') }}" class="navbar-brand">
-        <img src="/images/logo.svg" alt="Logo" />
-    </a>
-    <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarResponsive"
-    >
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="{{ route('home') }}" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('categories') }}" class="nav-link">Categories</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Rewards</a>
-            </li>
-            @guest
+        <a href="{{ route('home') }}" class="navbar-brand">
+            <img class="logo" src="/images/logo-hdrplnt.svg" alt="Logo" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('categories') }}" class="nav-link">Kategori</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Forum</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Blog</a>
+                </li>
+                @guest
                 <li class="nav-item">
                     <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
                 </li>
                 <li class="nav-item">
-                    <a
-                    href="{{  route('login') }}"
-                    class="btn btn-success nav-link px-4 text-white"
-                    >Sign In</a
-                    >
+                    <a href="{{  route('login') }}" class="btn btn-success nav-link px-4 text-white">Sign In</a>
                 </li>
-            @endguest
-        </ul>
+                @endguest
+            </ul>
 
-        @auth
+            @auth
             <!-- Desktop Menu -->
             <ul class="navbar-nav d-none d-lg-flex">
                 <li class="nav-item dropdown">
-                    <a
-                        href="#"
-                        class="nav-link"
-                        id="navbarDropdown"
-                        role="button"
-                        data-toggle="dropdown"
-                    >
-                        <img
-                            src="/images/icon-user.png"
-                            alt=""
-                            class="rounded-circle mr-2 profile-picture"
-                        />
+                    <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                        <img src="/images/icon-user.png" alt="" class="rounded-circle mr-2 profile-picture" />
                         Hi, {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu">
@@ -63,25 +44,24 @@
                             Settings
                         </a>
                         <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                               Logout
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('cart') }}" class="nav-link d-inline-block mt-2">
                         @php
-                            $carts = \App\Cart::where('users_id', Auth::user()->id)->count();
+                        $carts = \App\Cart::where('users_id', Auth::user()->id)->count();
                         @endphp
                         @if($carts > 0)
-                            <img src="/images/icon-cart-filled.svg" alt="" />
-                            <div class="card-badge">{{ $carts }}</div>
+                        <img src="/images/icon-cart-filled.svg" alt="" />
+                        <div class="card-badge">{{ $carts }}</div>
                         @else
-                            <img src="/images/icon-cart-empty.svg" alt="" />
+                        <img src="/images/icon-cart-empty.svg" alt="" />
                         @endif
                     </a>
                 </li>
@@ -98,9 +78,9 @@
                         Cart
                     </a>
                 </li>
-            </ul>    
-        @endauth
-        
-    </div>
+            </ul>
+            @endauth
+
+        </div>
     </div>
 </nav>
